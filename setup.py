2e8b46e86d764d2ab79346e211ac265e6e24ae80
Josh Hixson <josh@octanelending.com>
2018-03-17 14:09:50 -0400

    get full requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 import os
-from setuptools import setup
+from setuptools import setup,find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "lpconnector"
-    verison = "0.1"
-    url = "https://github.com/OctaneLending/lastpass-ldap-syc"
-    author = "Joshua Marcus-Hixson"
-    author_email = "josh@octanelending.com"
-    description = "Connect remote LDAP to LastPass Enterprise API"
-    packages=['lpconnector']
-    long_description=read('README.rst')
+    name="lpconnector",
+    version="0.1.0",
+    url="https://github.com/OctaneLending/lastpass-ldap-syc",
+    author="Joshua Marcus-Hixson",
+    author_email="josh@octanelending.com",
+    description= "Connect remote LDAP to LastPass Enterprise API",
+    install_requires=[
+        'python-ldap==3.0.0',
+        'python-dotenv==0.8.2',
+        'requests==2.18.4',
+        'docopt==0.6.2',
+    ],
+    packages=find_packages(exclude=['docs','tests']),
+    long_description=read('README.rst'),
+    entry_points = {
+        'console_scripts': [
+            'lpconnector=lpconnector.lpconnector:main',
+        ],
+    },
+)
